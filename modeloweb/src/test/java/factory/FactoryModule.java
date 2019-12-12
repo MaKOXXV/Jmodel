c950a169943023a424347e79c7018b28b4d35ac0
MaKOXXV <mlemos@indracompany.com>
2019-12-12 11:35:17 -0300

    commi

--- modeloweb/src/test/java/factory/FactoryModule.java
+++ modeloweb/src/test/java/factory/FactoryModule.java
@@ -28,45 +28,42 @@ public class FactoryModule {
 	@SuppressWarnings("deprecation")
 	public WebDriver iniciacao() {
 
-		switch (browser) {
-		case "Google":
+		if (browser.equals("Google")) {
 			System.out.println("|---------> Instanciando Driver : ChromeDriver <------------|");
 			System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 			driver = new ChromeDriver();
-			break;
-		case "FireFox":
+		}
+		if (browser.equals("FireFox")) {
 			System.out.println("|---------> Instanciando Driver : FireFox <------------|");
-			 System.setProperty("webdriver.gecko.driver","src/test/resources/geckodriver.exe");
-			 DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			 capabilities.setCapability("marionette",true);
-			 driver= new FirefoxDriver(capabilities);
-			 break;
-		case "IExplore":
+			System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			capabilities.setCapability("marionette", true);
+			driver = new FirefoxDriver(capabilities);
+		}
+		if (browser.equals("IExplore")) {
 			System.out.println("|---------> Instanciando Driver : Internet Explore <------------|");
 			System.setProperty("webdriver.ie.driver", "src/test/resources/IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
-			break;
-		default:
+		} else {
 			System.out.println("Este Browser nao e valido");
 		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		return driver;
+
 	}
 
 	public void highLight(WebElement element, WebDriver driver) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		// Objeto que permite alteracao de cor entre labels na estrutura selecionada
 		js.executeScript("arguments[0].setAttribute('style', 'background: blue; border: 2px solid green;');", element);
-		
-		
+
 	}
-	
+
 	public void screenShot(Scenario scenario) {
 		byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 		scenario.embed(screenshotBytes, "image/png");
 	}
-	
 
 	public void waitingElemnt(WebDriver driver, String findex) {
 		try {
